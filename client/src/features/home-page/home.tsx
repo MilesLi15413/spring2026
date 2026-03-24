5d08a2b532d144937eaf3db008ae8a09cd054195
chheangg <balldremz@gmail.com>
2026-03-24 01:55:16 -0700

    feat: remove apply button for now

--- client/src/features/home-page/home.tsx
+++ client/src/features/home-page/home.tsx
@@ -1,31 +1,47 @@
 import Image from "next/image";
-import Logo from "../../../public/logo.png"
-import HotAirBalloon from "../../../public/Hot Air Balloon.webp"
+import Logo from "../../../public/logo.png";
+import HotAirBalloon from "../../../public/Hot Air Balloon.webp";
 import { Homebg } from "./components/homebg";
 import { FrontPagePrimaryLayout } from "@/layouts/front-page-layout";
-import PinkPawL from "../../../public/Pink Paw.webp"
-import PinkPawR from "../../../public/Pink Paw 2.webp"
+import PinkPawL from "../../../public/Pink Paw.webp";
+import PinkPawR from "../../../public/Pink Paw 2.webp";
 import { Interest } from "./components/interest";
 import { ApplyButton } from "@/components/navbar";
 
 // TODO-IF-ACTION: make apply button active
 export default function Home() {
-    return(
-        <FrontPagePrimaryLayout className="flex bg-bgpurple overflow-x-clip text-white">
-            <div className="flex flex-col justify-center items-center mx-auto">
-                <div className="relative flex">
-                    <Image className="z-10 w-auto h-80 sm:h-80 md:h-80 lg:h-[24rem] 2xl:h-[29rem]" src={Logo} alt="HackCC Logo " priority sizes="(min-width: 1540px) 580px, (min-width: 1024px) 500px, (min-width: 640px) 410px, 310px"></Image>
-                    <Image className="-right-12 sm:-right-32 md:-right-40 2xl:-right-80 bottom-1/4 z-10 absolute w-auto h-28 sm:h-32 md:h-36 2xl:h-40 animate-bobbing ease-linear" src={HotAirBalloon} alt="Hot Air Balloon" sizes="(min-width: 1540px) 116px, (min-width: 780px) 104px, (min-width: 640px) 93px, 81px" priority></Image>
-                </div>
-                <div className="z-10 flex flex-col -mt-10 mb-6 text-center">
-                    <p className="font-bagel text-2xl sm:text-3xl md:text-4xl">May 2026</p>
-                    <p className="mt-2 font- font-mont text-base sm:text-lg md:text-xl text-right">Torrance &amp; San Mateo</p>
-                </div>
-                {/* <Interest></Interest> */}
-                <ApplyButton text="Apply to Attend" size="xl"></ApplyButton>
-            </div>
-        </FrontPagePrimaryLayout>
-    )
+  return (
+    <FrontPagePrimaryLayout className="flex bg-bgpurple overflow-x-clip text-white">
+      <div className="flex flex-col justify-center items-center mx-auto">
+        <div className="relative flex">
+          <Image
+            className="z-10 w-auto h-80 sm:h-80 md:h-80 lg:h-[24rem] 2xl:h-[29rem]"
+            src={Logo}
+            alt="HackCC Logo "
+            priority
+            sizes="(min-width: 1540px) 580px, (min-width: 1024px) 500px, (min-width: 640px) 410px, 310px"
+          ></Image>
+          <Image
+            className="-right-12 sm:-right-32 md:-right-40 2xl:-right-80 bottom-1/4 z-10 absolute w-auto h-28 sm:h-32 md:h-36 2xl:h-40 animate-bobbing ease-linear"
+            src={HotAirBalloon}
+            alt="Hot Air Balloon"
+            sizes="(min-width: 1540px) 116px, (min-width: 780px) 104px, (min-width: 640px) 93px, 81px"
+            priority
+          ></Image>
+        </div>
+        <div className="z-10 flex flex-col -mt-10 mb-6 text-center">
+          <p className="font-bagel text-2xl sm:text-3xl md:text-4xl">
+            May 2026
+          </p>
+          <p className="mt-2 font- font-mont text-base sm:text-lg md:text-xl text-right">
+            Torrance &amp; San Mateo
+          </p>
+        </div>
+        {/* <Interest></Interest> */}
+        <ApplyButton text="Apply to Attend" size="xl" disabled></ApplyButton>
+      </div>
+    </FrontPagePrimaryLayout>
+  );
 }
 
 // animate-swaying
